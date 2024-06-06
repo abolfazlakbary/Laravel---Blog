cfb126fd70d79ab609c20bd7cc4400687344b89a
unknown <abolfazl.akbary132@gmail.com>
2024-06-06 20:33:52 +0330

    routes configured

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ReplyController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +12,22 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => 'auth'], function()
+    {
+        Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+        Route::resource('posts', PostController::class);
+
+        Route::group(['prefix' => '/commnets', 'as' => 'comments.'], function()
+            {
+                Route::post('/{post}', [CommentController::class, 'store'])->name('store');
+            }
+        );
+
+        Route::group(['prefix' => '/replies', 'as' => 'replies.'], function()
+            {
+                Route::post('/{comment}', [ReplyController::class, 'store'])->name('store');
+            }
+        );
+    }
+);
